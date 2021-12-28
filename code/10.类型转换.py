73e50a40294043248a0321ac7b7183a7dd739b42
xiaozhu <xiaozhu0420@Gmail.com>
2021-12-28 14:08:14 +0800

    10-15

---
+++ code/10.类型转换.py
@@ -0,0 +1,10 @@
+a = 'hello'
+b = 123
+# print(a+b)
+# 类型转换
+# 类型转换不是改变对象本身的类型，而是根据当前对象的值创建一个新对象
+# int float str
+# int()整型函数
+a = '123'
+a = int(a)
+print(type(a))
---
+++ code/13.关系运算符.py
@@ -0,0 +1,16 @@
+# 关系运算
+# 关系成立返回True，否则False
+# >
+# <
+# >=
+# <=
+# ==
+# !=
+
+result = '2' > '10'#True
+# 比较unicode编码
+
+# is 比较两个对象是否是同一个对象，比较的是id
+# is not 同理
+a = 1 is True
+print(a)#False
